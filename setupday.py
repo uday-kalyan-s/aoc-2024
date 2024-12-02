43514f3daf4ae5046e8fc94d0063958a98b112ba
uday-kalyan-s <udaykalyansreenivasa@gmail.com>
2024-12-03 01:40:17 +0530

    added setup files

---
+++ setupday.py
@@ -0,0 +1,28 @@
+#!python
+
+from requests import get
+import sys
+import json
+import os
+
+day = sys.argv[1]
+
+cookie = json.loads(open("secrets.json").read())["cookie"]
+print("hello")
+fldr = f"solns/day{day}"
+
+os.mkdir(fldr)
+os.mkdir(fldr+"/pysolns")
+os.mkdir(fldr+"/haskellsolns")
+
+r = get(f"https://adventofcode.com/2024/day/{day}/input", cookies={"session": cookie}, headers={"User-Agent": "personal use fetcher by udaykalyansreenivasa@gmail.com, https://github.com/coder11235/aoc-2024/blob/main/setupday.py"})
+
+print(r.text)
+
+fl = open(f"{fldr}/input.txt", "w")
+fl.write(r.text)
+
+# one file solutions for the first few days
+open(f"{fldr}/pysolns/part1.py", "w")
+open(f"{fldr}/pysolns/part2.py", "w")
+open(f"{fldr}/sample.txt", "w")
